0433c20388997c77552e769aaec1066138c1e5f8
Zhan Knight <zhanknight@evans-macbook-pro.local>
2017-09-13 11:47:48 +0700

    Initial Commit

---
+++ SMMTZC - Simple Menubar Multi Time Zone Clock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SMMTZC - Simple Menubar Multi Time Zone Clock
+//
+//  Created by Zhan Knight on 9/13/17.
+//  Copyright © 2017 Zhan Knight. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
