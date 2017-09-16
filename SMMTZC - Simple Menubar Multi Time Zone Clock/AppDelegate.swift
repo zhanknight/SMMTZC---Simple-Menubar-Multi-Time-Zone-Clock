78981bb007f11d6128ad768fe8b8df8610057879
Zhan Knight <zhanknight@evans-macbook-pro.local>
2017-09-16 13:23:05 +0700

    Fundamental work. Removed visibility of default menubar, created the basic display and added a basic dropdown menu with a 'quit' option.

--- SMMTZC - Simple Menubar Multi Time Zone Clock/AppDelegate.swift
+++ SMMTZC - Simple Menubar Multi Time Zone Clock/AppDelegate.swift
@@ -11,10 +11,31 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    // Create something for the statusbar
+    var statusDisplay : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        // Make an object of it and give it a variable length
+        statusDisplay = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        
+        // and go ahead and set that object's title
+        statusDisplay?.title = "SMMTZC"
+        
+        //
+        // Create a simple drop dpwn menu for the display
+        //
+        
+        let dropMenu = NSMenu()
+//        dropMenu.addItem(NSMenuItem(title: "Not Yet", action: #selector(AppDelegate.testMe), keyEquivalent: ""))
+        dropMenu.addItem(NSMenuItem(title: "Quit SMMTZC", action: #selector(AppDelegate.quitApp), keyEquivalent: "q"))
+        
+        statusDisplay?.menu = dropMenu
+        
+    }
+    
+    func quitApp() {
+        NSApplication.shared().terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
