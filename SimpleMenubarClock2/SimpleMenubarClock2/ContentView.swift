b085a759f69dfba461ab9d67dee1e284ebaae1c7
zhanknight <zhanknight@gmail.com>
2020-03-27 19:55:08 -0400

    Beginning update to Swift 5

---
+++ SimpleMenubarClock2/SimpleMenubarClock2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SimpleMenubarClock2
+//
+//  Created by Zhan Knight on 3/27/20.
+//  Copyright © 2020 Zhan Knight. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
