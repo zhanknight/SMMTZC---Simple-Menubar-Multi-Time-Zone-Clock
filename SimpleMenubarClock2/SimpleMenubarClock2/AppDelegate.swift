bd37ae6488802661cc13b4062f0cbcd6415adab2
zhanknight <zhanknight@gmail.com>
2020-04-13 00:38:17 -0400

    Implemented basic submenu, still not functional for choosing timezone.

--- SimpleMenubarClock2/SimpleMenubarClock2/AppDelegate.swift
+++ SimpleMenubarClock2/SimpleMenubarClock2/AppDelegate.swift
@@ -14,46 +14,37 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarItem: NSStatusItem!
     var clockTimer: Timer?
+    // ADD GLOBAL TIMEZONE ABBREV VAR HERE?!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
+        
         let statusBar = NSStatusBar.system
         statusBarItem = statusBar.statusItem(
             withLength: NSStatusItem.variableLength)
         statusBarItem.button?.title = "SMMTZ"
+        
+        
+        let statusBarMenu = NSMenu()
+        let subOptionsMenu = NSMenu()
+        let mainOptionsMenu = NSMenuItem(title: "Zones", action: nil, keyEquivalent: "")
+        statusBarMenu.addItem(mainOptionsMenu)
+        statusBarMenu.setSubmenu(subOptionsMenu, for: mainOptionsMenu)
 
-        let statusBarMenu = NSMenu(title: "SMMTZ Menu Bar")
+        // on clicking these options, need to set the timezone abbreviation and pass to clockdisplay()
+        subOptionsMenu.addItem(NSMenuItem(title: "Option 1", action: nil, keyEquivalent: ""))
+        subOptionsMenu.addItem(NSMenuItem(title: "Option 2", action: nil, keyEquivalent: ""))
+        
         statusBarItem.menu = statusBarMenu
-
-        // options not implemented yet
-        statusBarMenu.addItem(
-            withTitle: "Options..",
-            action: #selector(AppDelegate.smmtzOptions),
-            keyEquivalent: "O")
 
         statusBarMenu.addItem(
             withTitle: "Quit",
             action: #selector(AppDelegate.smmtzQuit),
             keyEquivalent: "Q")
         
-        
     // timer that updates clock display every 2 seconds
     // is there a better way to do this than a timer
     // calling clockDisplay() over and over again?
-        
-        clockTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(clockDisplay), userInfo: nil, repeats: true)
-    
-    }
-
-
-    @objc func smmtzOptions() {
-        print("Options menu here!")
-    }
-
-
-    @objc func smmtzQuit() {
-        print("TIME IS A HUMAN CONSTRUCT")
-        NSApplication.shared.terminate(self)
+            clockTimer = Timer.scheduledTimer(timeInterval: 2, target: self,selector:#selector(clockDisplay), userInfo: nil, repeats: true)
     }
    
   // this is the actual clock display
@@ -61,12 +52,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   // TODO: Make this dynamic, user selectable!
     
     @objc func clockDisplay() {
-        let indoTime = DateFormatter()
-        indoTime.timeZone = TimeZone(abbreviation: "ICT")
-        indoTime.timeStyle = .short
-        statusBarItem.button?.title = "Indonesia: \(indoTime.string(from:Date()))"
+        let time = DateFormatter()
+        time.timeZone = TimeZone(abbreviation: "ICT")
+        time.timeStyle = .short
+        statusBarItem.button?.title = "\(time.string(from:Date()))"
      }
 
+    @objc func smmtzQuit() {
+           print("TIME IS A HUMAN CONSTRUCT")
+           NSApplication.shared.terminate(self)
+       }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
