de8f851c74b82dd1f7347a51cc2ca45e727a83c5
zhanknight <zhanknight@gmail.com>
2022-07-18 21:09:12 -0400

    Remove non functional hotkey bindings, closes #8

--- SimpleMenubarClock2/SimpleMenubarClock2/AppDelegate.swift
+++ SimpleMenubarClock2/SimpleMenubarClock2/AppDelegate.swift
@@ -1,57 +1,46 @@
 //
 //
 //  SimpleMenubarClock2
-//  Created by zhanknight
+//  Created by Zhan Knight
 //
 //
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var statusBarItem: NSStatusItem!
-    var clockTimer: Timer?
-    var selectedZone = "SMMTZ"
-    var tzName: String = "SMMTZ"
+    private var statusBarItem: NSStatusItem!
+    private var clockTimer: Timer?
+    private var selectedZone = "SMMTZ"
+    private var tzName: String = "SMMTZ"
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
         let statusBar = NSStatusBar.system
-        statusBarItem = statusBar.statusItem(
-            withLength: NSStatusItem.variableLength)
-        statusBarItem.button?.title = selectedZone
-        
+            statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+            statusBarItem.button?.title = selectedZone
         let statusBarMenu = NSMenu()
         let subOptionsMenu = NSMenu()
         let mainOptionsMenu = NSMenuItem(title: "Zones", action: nil, keyEquivalent: "")
-        statusBarMenu.addItem(mainOptionsMenu)
-        statusBarMenu.setSubmenu(subOptionsMenu, for: mainOptionsMenu)
-        
+            statusBarMenu.addItem(mainOptionsMenu)
+            statusBarMenu.setSubmenu(subOptionsMenu, for: mainOptionsMenu)
+    // get all possible time zones
         let tzList = TimeZone.abbreviationDictionary
-
-        tzList.forEach { key, value in
-            subOptionsMenu.addItem(NSMenuItem(title: "\(value)", action: #selector(setNewZone), keyEquivalent: "\(key)"))
-        }
-        
-        statusBarItem.menu = statusBarMenu
-
-        statusBarMenu.addItem(
-            withTitle: "Quit",
-            action: #selector(AppDelegate.smmtzQuit),
-            keyEquivalent: "Q")
-        
+    // iterate over time zones and add menu item for each
+            tzList.forEach { key, value in
+                subOptionsMenu.addItem(NSMenuItem(title: "\(value)", action: #selector(setNewZone), keyEquivalent: ""))
+            }
+    // add a quit button that kills the app
+            statusBarItem.menu = statusBarMenu
+                statusBarMenu.addItem(withTitle: "Quit", action: #selector(AppDelegate.smmtzQuit), keyEquivalent: "Q")
     // timer that updates clock display every 2 seconds
-    // is there a better way to do this than a timer
-    // calling clockDisplay() over and over again?
-        clockTimer = Timer.scheduledTimer(timeInterval: 2, target: self,selector: #selector(clockDisplay), userInfo: nil, repeats: true)
-    }
-   
-    
-    @objc func setNewZone( sender : NSMenuItem ) {
-        selectedZone = sender.keyEquivalent
+            clockTimer = Timer.scheduledTimer(timeInterval: 2, target: self,selector: #selector(clockDisplay), userInfo: nil, repeats: true)
+            }
+    // func to handle time zone menu changes
+    @objc func setNewZone( sender : NSMenuItem) {
+        selectedZone = sender.title
         tzName = sender.title
     }
-    
+    // func that handles what to display on clock from initial open to zone selection
     @objc func clockDisplay() {
         if selectedZone == "SMMTZ" {
             selectedZone = "Choose Timezone.."
@@ -62,17 +51,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
             else {
                 let time = DateFormatter()
-                time.timeZone = TimeZone(abbreviation: selectedZone)
+                time.timeZone = TimeZone(identifier: selectedZone)
                 time.timeStyle = .short
                 statusBarItem.button?.title = "\(tzName): \(time.string(from:Date()))"
-     }
-    }
-    
+        }
+        }
+    // the quit func
     @objc func smmtzQuit() {
            print("TIME IS A HUMAN CONSTRUCT")
            NSApplication.shared.terminate(self)
        }
-    
     func applicationWillTerminate(_ aNotification: Notification) {
     }
 }
